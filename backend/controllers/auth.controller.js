ba22ea43cb7d65e7c573f06f588257cbb536c91a
Usman Habib <usmanhabib802@gmail.com>
2025-08-10 01:37:10 +0500

    changes in auth route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,9 @@
-export const signup = (req, res) => {
-  res.send("Signup User");
-  console.log("signupUser");
+export const signup = async(req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+  } catch (error) {
+    
+  }
 };
 
 export const login = (req, res) => {
