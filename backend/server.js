317aa6e5a32d5da020fa045d23c4e5ee6d4fe8ab
Usman Habib <usmanhabib802@gmail.com>
2025-08-10 01:26:47 +0500

    Auth Routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,22 @@
-    // import express from "express";
-    // import dotenv from "dotenv";
-    // const app = express();
-    // dotenv.config();
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
-    // const port = process.env.PORT || 5000;
-
-    // app.get("/", (req, res) => {
-    // res.send("Hello World!!!!");
-    // });
-
-    // app.listen(5000, () => console.log(`Server is running on port {PORT}`));
-
-
-    const express = require("express");
-const dotenv = require("dotenv");
+const app = express();
 
 dotenv.config();
-const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
+// Default test route
 app.get("/", (req, res) => {
-  res.send("Hello World!!!!");
+  res.send("Hello World!!");
 });
+
+// Auth routes
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
